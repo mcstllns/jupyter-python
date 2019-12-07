ea2ffa044977fbc84e626d4b833394e057799e7c
mcstllns <castellanos.miguel.angel@gmail.com>
2019-12-07 13:01:08 +0100

    nada nuevo en realidad

--- addusers.py
+++ addusers.py
@@ -5,21 +5,24 @@
 # ------------------------------------------------------
 
 #
-# Este codigo se creo para gestionar jupyterHub, lo que hace es manetener actualizados los
-# directorios de los usuarios
+# Este codigo se creo para gestionar jupyterHub, lo que hace es crear usuarios
 
 # > addusers.py fileWithUserNames
 
-# Cuando se ejecuta va a borrar lo que hay en el home de los usuarios
-# de jupyter y poner lo que hay en la carpeta path/to/src
 
 import os
 
+# filename = "/home/miguel/Desktop/Desarrollo/pruebasPy/usersList.txt"
 
-#f = open("/home/miguel/Desktop/Desarrollo/pruebasPy/usersList.txt", "r")
 
-filename = "/home/miguel/Desktop/Desarrollo/pruebasPy/usersList.txt"
+if len(sys.argv)-1 != 1:
+    print('ERROR: Sintaxis: {} fileWithUserNames'.format(sys.argv[0])) 
+    sys.exit()
 
+
+filename = sys.argv[1]
+
+print("-----------------------------------")
 
 with open(filename) as f:
     content = f.readlines()
--- p2.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-
-f = open("demofile3.txt", "a")
-f.write("%s %d\n" % ("casa", 23))
-f.close()
